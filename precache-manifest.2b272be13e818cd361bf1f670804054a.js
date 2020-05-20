833b0d01f67c89005a226ea791905c6886731a8e
Vvk177 <vivekjhd177@gmail.com>
2020-05-20 07:01:28 +0530

    Updates

---
+++ precache-manifest.2b272be13e818cd361bf1f670804054a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d315273dcfb38ec5eade7ab4ec71805",
+    "url": "/basic/index.html"
+  },
+  {
+    "revision": "95179af93519f75650e9",
+    "url": "/basic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "853035305fb6e73ef703",
+    "url": "/basic/static/js/2.c95dedfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basic/static/js/2.c95dedfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95179af93519f75650e9",
+    "url": "/basic/static/js/main.6c3b4f34.chunk.js"
+  },
+  {
+    "revision": "958baf6e554a5b19ac43",
+    "url": "/basic/static/js/runtime-main.138a4d32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basic/static/media/logo.5d5d9eef.svg"
+  }
+]);
